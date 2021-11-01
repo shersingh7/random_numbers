8fef80dac990c594a6752429e58a382bc25457bd
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-11-01 15:26:24 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+# for i in range(10):
+#     print(random.random())
+#
+# for i in range(10):
+#     print(random.randint(10,25))
+
+members = ['Mihir','Davinder','Bhavya','Sid','Kunal','UTK']
+print(random.choice(members))
+
